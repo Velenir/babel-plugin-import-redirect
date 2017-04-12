17e05b005c18f414754900f166a37f546a2ae468
Velenir <Velenir@users.noreply.github.com>
2017-04-12 19:07:41 +0300

    test(example): Add noop export test

--- __tests__/exportFrom.test.js
+++ __tests__/exportFrom.test.js
@@ -11,3 +11,15 @@ describe('export from', () => {
 		return compareTranspiled("examples/exportFrom/relative.js", options);
 	});
 });
+
+describe('export', () => {
+	test('should not change path', () => {
+		const options = {
+			redirect: {
+				"/examples(/\\w+)*/lib\\.js$" : "./examples/exportFrom/different/lib"
+			}
+		};
+		
+		return compareTranspiled("examples/exportFrom/noop.js", options);
+	});
+});
