51484f52a398280b75be49e3851f65eebc2057b3
Velenir <Velenir@users.noreply.github.com>
2017-04-12 15:36:32 +0300

    chore(examples): Better correspond to transpiled end result

--- examples/import/relative_transpiled.js
+++ examples/import/relative_transpiled.js
@@ -1,4 +1,2 @@
-"use strict";
-
 // eslint-disable-next-line no-unused-vars
 import lib from "./different/lib";
--- examples/require/relative_transpiled.js
+++ examples/require/relative_transpiled.js
@@ -1,4 +1,2 @@
-"use strict";
-
 // eslint-disable-next-line no-unused-vars
 const lib = require("./different/lib");
