50e0208cedf480286ba6d755ac2aaf881b4108af
Velenir <Velenir@users.noreply.github.com>
2017-04-12 22:14:16 +0300

    feat(redirect): Allow to use groups ($1) in redirect target

--- src/helpers/matchRedirect.js
+++ src/helpers/matchRedirect.js
@@ -2,9 +2,21 @@ import {resolve} from "path";
 
 export default function (filename, regexps, root) {
 	for(let redirectPair of regexps) {
-		if(redirectPair[0].test(filename)) {
-			console.log("FOUND MATCH", redirectPair[0]);
-			return resolve(root, redirectPair[1]);
+		const regexp = redirectPair[0];
+		
+		if(regexp.test(filename)) {
+			console.log("FOUND MATCH", regexp);
+			let redirect = redirectPair[1];
+			
+			// if redirect is of "different/path/$1.js" form
+			if(/\$\d/.test(redirect)) {
+				// "abs/path/to/path/lib.js".match(/path/(\w+).js$/)[0] ->
+				// "path/lib.js".replace(/path/(\w+).js$/, "different/path/$1.js") ->
+				// "different/path/lib.js"
+				redirect = filename.match(regexp)[0].replace(regexp, redirect);
+			}
+			
+			return resolve(root, redirect);
 		}
 	}
 	
