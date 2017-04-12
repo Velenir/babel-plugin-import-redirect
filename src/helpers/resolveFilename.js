444bc686f8e9c406db540e399af87c2ddaca14ae
Velenir <Velenir@users.noreply.github.com>
2017-04-12 18:09:16 +0300

    feat(resolve): Cache resolve call results

--- src/helpers/resolveFilename.js
+++ src/helpers/resolveFilename.js
@@ -1,9 +1,40 @@
 import resolve from "resolve";
-import path from "path";
+import {dirname} from "path";
+
+const cache = {};
 
 export default function (fromFile, filename, extensions) {
-	return resolve.sync(filename, {
-		basedir: path.dirname(fromFile),
-		extensions
-	});
+	const dir = dirname(fromFile);
+	const cached = cache[dir];
+	
+	if(cached) {
+		const withFilename = cached[filename];
+		if(withFilename) {
+			let resolved = withFilename.get(extensions);
+			if(resolved !== undefined) return resolved;
+			
+			resolved = getResolved();
+			withFilename.set(extensions, resolved);
+			return resolved;
+		}
+		
+		const resolved = getResolved();
+		cached[filename] = new Map().set(extensions, resolved);
+		return resolved;
+	}
+	
+	const resolved = getResolved();
+	cache[dir] = {
+		[filename]: new Map().set(extensions, resolved)
+	};
+	
+	return resolved;
+	
+	
+	function getResolved(){
+		return resolve.sync(filename, {
+			basedir: dir,
+			extensions
+		});
+	}
 }
