6f1ba3e83d9375b92c63b3a5e43f0976eee5a0e8
Velenir <Velenir@users.noreply.github.com>
2017-04-12 19:08:32 +0300

    fix(transformers): Fix noop export breaking on null source node

--- src/transformers/importExportDeclaration.js
+++ src/transformers/importExportDeclaration.js
@@ -2,7 +2,7 @@ import replacePath from "../helpers/replacePath";
 
 export default function (t, path, state, regexps) {
 	const sourcePath = path.get("source");
-	if(sourcePath) {
+	if(sourcePath.node) {
 		replacePath(t, sourcePath, state, regexps);
 	}
 }
