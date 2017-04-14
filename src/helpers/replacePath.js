6c8ce552cf95452c35a9e5d4a322960bfdf94572
Velenir <Velenir@users.noreply.github.com>
2017-04-14 18:50:37 +0300

    fix(replacement): Fix file extension added when redirect didn't have one

--- src/helpers/replacePath.js
+++ src/helpers/replacePath.js
@@ -1,6 +1,6 @@
 import resolveNode from "../helpers/resolveNode";
 import match from "../helpers/matchRedirect";
-import {relative, dirname} from "path";
+import {relative, dirname, extname} from "path";
 
 export default function (t, originalPath, {opts: {root, extensions}, file: {opts: {filename}}}, regexps) {
 	const requiredFilename = resolveNode(dirname(filename), originalPath.node.value, extensions);
@@ -26,6 +26,11 @@ export default function (t, originalPath, {opts: {root, extensions}, file: {opts
 		}
 		let relativeRedirect = relative(dirname(filename), redirected);
 		if(!relativeRedirect.startsWith(".")) relativeRedirect = "./" + relativeRedirect;
+		
+		if(!extname(redirect)) {
+			const ext = extname(relativeRedirect);
+			if(ext) relativeRedirect = relativeRedirect.slice(0, -ext.length);
+		}
 		
 		originalPath.replaceWith(t.stringLiteral(relativeRedirect));
 	}
