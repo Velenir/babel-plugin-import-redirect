df48fa2233d82cfe81da57f272ef66f8dcc120a6
Velenir <Velenir@users.noreply.github.com>
2017-04-18 22:34:33 +0300

    feat(transformers): Allow to transform dynamic import
    Only with a syntax-dynamic-import supplied

--- src/transformers/requireCall.js
+++ src/transformers/requireCall.js
@@ -3,7 +3,8 @@ import replacePath from "../helpers/replacePath";
 export default function (t, path, state, {regexps, functionNames}) {
 	console.log("CHECKING", path.node.callee.name);
 	console.log("IN", functionNames);
-	if(functionNames.has(path.node.callee.name)) {
+	const {callee} = path.node;
+	if(functionNames.has(callee.name) || t.isImport(callee)) {
 		// console.log(__dirname, __filename);
 		console.log(state.file.opts.filename);
 		console.log(state.file.opts.basename);
