8322f9f3cbf29e6c05862684610382d5965461ad
Velenir <Velenir@users.noreply.github.com>
2017-04-19 12:35:01 +0300

    feat(transformers): Allow to replace path inside custom Object.method calls

--- src/transformers/requireCall.js
+++ src/transformers/requireCall.js
@@ -1,16 +1,22 @@
 import replacePath from "../helpers/replacePath";
 
+const membToString = (t, {object, property, computed}) => {
+	const propIsIdentifier = t.isIdentifier(property);
+	if(!object.name || (computed && propIsIdentifier)) return null;
+	return object.name + "." + (propIsIdentifier ? property.name : property.value);
+};
+
 export default function (t, path, state, {regexps, functionNames}) {
 	console.log("CHECKING", path.node.callee.name);
 	console.log("IN", functionNames);
 	const {callee} = path.node;
-	if(functionNames.has(callee.name) || t.isImport(callee)) {
-		// console.log(__dirname, __filename);
+	if((t.isIdentifier(callee) && functionNames.has(callee.name)) ||
+		t.isImport(callee) ||
+		(t.isMemberExpression(callee) && functionNames.has(membToString(t, callee)))
+	) {
 		console.log(state.file.opts.filename);
 		console.log(state.file.opts.basename);
 		console.log(state.file.opts.filenameRelative);
-		console.log(path.node.arguments.length);
-		
 		
 		const argPath = path.get("arguments.0");
 		if(argPath.isStringLiteral()) {
