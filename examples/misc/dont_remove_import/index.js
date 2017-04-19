d94ca38878711783b0c0a3ed04fd00d5b1b4d438
Velenir <Velenir@users.noreply.github.com>
2017-04-19 17:25:56 +0300

    chore(examples): Add examples where import won't be removed

---
+++ examples/misc/dont_remove_import/index.js
@@ -0,0 +1,12 @@
+/*eslint-disable no-undef, no-unused-vars*/
+export { default as styles0 } from "./style.css";
+
+import styles1 from "./style.css";
+
+const styles2 = require("./style.css");
+
+const styles3 = import("./style.css");
+
+const styles4 = custom_require_function("./style.css");
+
+const styles5 = Object.method("./style.css");
