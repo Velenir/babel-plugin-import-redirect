8d95ac919ded279473c2f7a9834bab9dc1f95d32
Velenir <Velenir@users.noreply.github.com>
2017-04-20 22:00:49 +0300

    chore(examples): Add example of removing unassigned (without side effects) imports when redirect is

---
+++ examples/misc/dont_replace_import_but_remove/index.js
@@ -0,0 +1,11 @@
+/*eslint-disable no-undef, import/no-duplicates*/
+import "./style.css";
+import {} from "./style.css";
+
+require("./style.css");
+
+import("./style.css");
+
+custom_require_function("./style.css");
+
+Object.method("./style.css");
